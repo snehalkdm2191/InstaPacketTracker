aa9d71bc1b472df2f3149ceca54fff4e7fdc0759
snehalkdm2191 <snehalkdm2191@gmail.com>
2021-09-23 16:18:32 +0530

    style changes done.

--- src/component/ParcelModal.jsx
+++ src/component/ParcelModal.jsx
@@ -3,18 +3,19 @@ import React, { useState } from "react";
 import ModalDataTable from "./ModalDataTable";
 
 export default function ProjectModal({ parcel }) {
-  const [zoom, setZoom] = useState(4);
+  const [zoom] = useState(4);
   const options = `&output=embed&z=${zoom}`;
   const googleMaps = `https://maps.google.com/maps?q=${parcel.location_coordinate_latitude},${parcel.location_coordinate_longitude}&hl=en&z=14&amp${options}`;
   return (
     <div className="container">
       <div className="row">
+        <h3>Parcel Details</h3>
         <div className="col-md-6">
           <ModalDataTable parcel={parcel} />
         </div>
         <div className="col-md-6">
           <div id="frame-container">
-            <iframe className="map-frame" src={googleMaps}></iframe>
+            <iframe className="map-frame" src={googleMaps} title="Insta track map"></iframe>
           </div>
         </div>
       </div>
